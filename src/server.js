af588bf479600c3ac2314afc467801a43e93cf73
Nauval Labib Salim <nls35.prolink@gmail.com>
2025-11-11 16:51:48 +0700

    postgres database implementation

--- src/server.js
+++ src/server.js
@@ -1,10 +1,16 @@
+require("dotenv").config();
+
 const Hapi = require("@hapi/hapi");
-const routes = require("./routes");
+const notes = require("./api/notes");
+const NotesService = require("./services/postgres/NotesService");
+const NotesValidator = require("./validator/notes");
+const ClientError = require("./exceptions/ClientError"); // pastikan ini diimpor
 
 const init = async () => {
+  const notesService = new NotesService();
   const server = Hapi.server({
-    port: 5000,
-    host: "localhost",
+    port: process.env.PORT,
+    host: process.env.HOST,
     routes: {
       cors: {
         origin: ["*"],
@@ -12,10 +18,32 @@ const init = async () => {
     },
   });
 
-  server.route(routes);
+  await server.register({
+    plugin: notes,
+    options: {
+      service: notesService,
+      validator: NotesValidator,
+    },
+  });
 
-  await server.start();
-  console.log(`Server berjalan pada ${server.info.uri}`);
+  // Extension function untuk menangani ClientError
+  server.ext("onPreResponse", (request, h) => {
+    const { response } = request;
+
+    // Penanganan error dari kelas ClientError
+    if (response instanceof ClientError) {
+      const newResponse = h.response({
+        status: "fail",
+        message: response.message,
+      });
+      newResponse.code(response.statusCode);
+      return newResponse;
+    }
+
+    // Biarkan respons lanjut ke tahap berikutnya
+    return h.continue;
+  });
+r berjalan pada ${server.info.uri}`);
 };
 
 init();
