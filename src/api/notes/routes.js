931431641b3515ea1e9890f96cf8271e9a6f94a3
Nauval Labib Salim <nls35.prolink@gmail.com>
2025-11-21 20:08:01 +0700

    authentication implemented

--- src/api/notes/routes.js
+++ src/api/notes/routes.js
@@ -1,28 +1,43 @@
 const routes = (handler) => [
   {
-    method: 'POST',
-    path: '/notes',
+    method: "POST",
+    path: "/notes",
     handler: handler.postNoteHandler, // postNoteHandler hanya menerima dan menyimpan "satu" note.
+    options: {
+      auth: "notesapp_jwt",
+    },
   },
   {
-    method: 'GET',
-    path: '/notes',
+    method: "GET",
+    path: "/notes",
     handler: handler.getNotesHandler, // getNotesHandler mengembalikan "banyak" note.
+    options: {
+      auth: "notesapp_jwt",
+    },
   },
   {
-    method: 'GET',
-    path: '/notes/{id}',
+    method: "GET",
+    path: "/notes/{id}",
     handler: handler.getNoteByIdHandler, // getNoteByIdHandler mengembalikan "satu" note.
+    options: {
+      auth: "notesapp_jwt",
+    },
   },
   {
-    method: 'PUT',
-    path: '/notes/{id}',
+    method: "PUT",
+    path: "/notes/{id}",
     handler: handler.putNoteByIdHandler, // putNoteByIdHandler hanya menerima dan mengubah "satu" note.
+    options: {
+      auth: "notesapp_jwt",
+    },
   },
   {
-    method: 'DELETE',
-    path: '/notes/{id}',
+    method: "DELETE",
+    path: "/notes/{id}",
     handler: handler.deleteNoteByIdHandler,
+    options: {
+      auth: "notesapp_jwt",
+    },
   },
 ];
 
